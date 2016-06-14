f35e340a1198be474f066538d8fb34485c397ac6
Carl Van Haren <cwvanharen@gmail.com>
2016-06-14 12:50:53 -0700

    Completed Q8 Parsing

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,30 @@
-#The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
-# The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
-
-
+from sys import argv
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+script, filename = argv
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+def read_data(data):
+    '''Open csv file, read it and return a dictionary'''
+    
+    with open(data, 'rb') as football_csv:
+        football = csv.reader(football_csv)
+        fbdict = dict()
+        for thing in football:
+            fbdict[thing[0]] = thing[1:] 
+    del fbdict['Team']
+    return fbdict   
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_min_score_difference(parsed_data):
+    '''Takes dictionary and returns the team with smallest absolute goal difference'''
+    
+    dif = []
+    for team, values in parsed_data.iteritems():
+        dif.append((team, abs(int(values[4]) - int(values[5]))))
+    
+    minimum = min(dif, key=lambda dif: dif[1])
+    return minimum[0]
+ 
+    
+min_dif = get_min_score_difference(read_data(filename))
+print min_dif
